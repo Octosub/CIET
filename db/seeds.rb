b9873becb22a2d5da2e05fd41aca696f8ba415b9
Noah Endo <noah.eyeing@gmail.com>
2024-02-26 15:20:22 +0900

    added users with pfp to seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,20 @@ require 'faker'
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+require "open-uri"
 
-users = ['Koji', 'Felix', 'Leo', 'Noah']
+
+Food.destroy_all
+User.destroy_all
+
+users = ['imnadleeh', 'flxlng', 'leamuno', 'Octosub']
 
 users.each do |user_name|
   puts "creating #{user_name}"
   user = User.create(email: "#{user_name}@me.com", password: "123123")
+  file = URI.open("https://kitt.lewagon.com/placeholder/users/#{user_name}")
+  user.photo.attach(io: file, filename: "#{user_name}.jpg", content_type: "image/png")
+
   puts "creating dishes"
 
   2.times do
@@ -29,6 +37,14 @@ users.each do |user_name|
     puts "creating #{dish}"
     food = Food.new(name: dish, ingredient_list: ingredient_list )
     food.user = user
+
+    puts "attaching picture 1"
+    file = URI.open('https://isitveganjapan.files.wordpress.com/2019/02/20190207_174944.jpg')
+    food.photos.attach(io: file, filename: "onigiri_front.jpg", content_type: "image/png" )
+
+    puts "attaching picture 2"
+    file = URI.open('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTotMthTFHmevaDK1RGb4ZzSHr31MlDvKDIeg&s')
+    food.photos.attach(io: file, filename: "onigiri_back.jpg", content_type: "image/png" )
     food.save
   end
 end
