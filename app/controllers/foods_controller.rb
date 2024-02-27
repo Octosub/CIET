8defee6d3b58e8a69017d2295fc61410dccdcb74
Felix Lange <rayon-zunder.0u@icloud.com>
2024-02-27 15:16:03 +0900

    without double env

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,26 @@
 class FoodsController < ApplicationController
-  
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to food_path(@food)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, photos: [])
+  end
 end
