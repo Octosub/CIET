aa7633fd34549f29731ae48184cce915a44b73f7
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-02-28 15:24:14 +0900

    Basic filtering of text is completed

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,27 +4,24 @@ class Food < ApplicationRecord
 
   # validates :ingredient_list, presence: true
   # validates :photos, presence: true
-  before_save :extract_ingredients, :translate
+  # after_save :extract_ingredients, :translate
 
   def extract_ingredients
     require "./app/services/Ocr.rb"
     extracted_text = Ocr.extract_text(self.photos[0])
     formatted_text = extracted_text
     self.ingredient_list = formatted_text
-
+    save
   end
 
   def translate
-    require "google/cloud/translate/v2"
-    gtranslate_client = Google::Cloud::Translate::V2.new(
-      project_id: "grounded-elf-415603",
-      credentials: "./grounded-elf-415603-0893a7160822.json"
-     )
+    gtranslate_client = Google::Cloud::Translate::V2.new
     puts "translating..."
 
     translation = gtranslate_client.translate "#{ingredient_list}", to: "en"
     self.ingredient_list = translation
     puts "translation successfull, saving..."
+    save
   end
 
 end
--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,4 +1,5 @@
 require "google/cloud/vision"
+require "google/cloud/translate/v2"
 
 Google::Cloud::Vision.configure do |config|
   config.credentials = JSON.parse(ENV.fetch('VISION_CREDENTIALS'))
