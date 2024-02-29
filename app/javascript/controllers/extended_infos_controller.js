419b509775479221f07c031067e979349dbb5085
Felix Lange <rayon-zunder.0u@icloud.com>
2024-02-29 12:08:15 +0900

    Add new styles for backgrounds and update fonts

--- app/javascript/controllers/extended_infos_controller.js
+++ app/javascript/controllers/extended_infos_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   static targets = [ "info" ];
 
   connect() {
-    console.log("connected");
+    console.log("connected", this.infoTarget);
   }
 
   revealInfos(event) {
-    event.currentTarget.nextElementSibling.classList.toggle("d-none");
+    // event.currentTarget.nextElementSibling.classList.toggle("d-none");
     // console.log(this.infoTarget);
-    // this.infoTarget.classList.toggle("d-none");
+    this.infoTarget.classList.toggle("d-none");
     // event.currentTarget.innerHTML = event.currentTarget.innerHTML === "v" ? ">" : "v";
     event.currentTarget.classList.toggle("rotated-element")
   }
