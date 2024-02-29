49b24954ddb2ddd6637b38c388bf148fe6188f08
Noah Endo <noah.eyeing@gmail.com>
2024-02-29 17:46:43 +0900

    maybe heroku?

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,7 +1,7 @@
 require "google/cloud/vision"
 require "google/cloud/translate/v2"
 
-if Rails.environment.development?
+if Rails.env.development?
   Google::Cloud::Vision.configure do |config|
     config.credentials = JSON.parse(ENV.fetch('VISION_CREDENTIALS'))
   end
