23df8dab209b617c8547c679f0e88ac635cebc99
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-03-01 16:27:39 +0900

    Fixes seed issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,16 @@
 require "faker"
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   [“Action”, “Comedy”, “Drama”, “Horror”].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 require "open-uri"
+
 Food.destroy_all
 User.destroy_all
 Ingredient.destroy_all
 users = ["imnadleeh", "flxlng", "leamuno", "Octosub"]
 users.each do |user_name|
-  puts “creating #{user_name}”
+  puts "creating #{user_name}"
   user = User.create(email: "#{user_name}@me.com", password: "123123")
   file = URI.open("https://kitt.lewagon.com/placeholder/users/#{user_name}")
   user.photo.attach(io: file, filename: "#{user_name}.jpg", content_type: "image/png")
-  puts “creating dishes”
+  puts "creating dishes"
   2.times do
     ingredient_list = ""
     9.times do
@@ -31,10 +23,10 @@ users.each do |user_name|
     food.user = user
     puts "attaching picture 1"
     file = URI.open("https://isitveganjapan.files.wordpress.com/2019/02/20190207_174944.jpg")
-    food.photos.attach(io: file, filename: “onigiri_front.jpg”, content_type: “image/png” )
+    food.photos.attach(io: file, filename: "onigiri_front.jpg", content_type: "image/png" )
     puts "attaching picture 2"
     file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTotMthTFHmevaDK1RGb4ZzSHr31MlDvKDIeg&s")
-    food.photos.attach(io: file, filename: “onigiri_back.jpg”, content_type: “image/png” )
+    food.photos.attach(io: file, filename: "onigiri_back.jpg", content_type: "image/png" )
     food.save
   end
 end
