3ed292f213b0bef6242291edd9f6d1d16b5e21f0
Noah Endo <noah.eyeing@gmail.com>
2024-03-04 11:42:10 +0900

    loading screen

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -2,13 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="autosubmit"
 export default class extends Controller {
-  static targets = [ "form" ];
+  static targets = [ "form", "hide", "show" ];
 
   connect() {
     console.log("autosubmit connected");
+    console.log(this.formTarget, this.showTarget.innerHTML);
+
   }
 
   submitForm() {
+    this.hideTarget.classList.toggle("d-none");
+    this.showTarget.classList.remove("d-none");
+    this.formTarget.submit();
+  }
+
+  submitPicture() {
     console.log(this.formTarget);
     this.formTarget.submit();
   }
