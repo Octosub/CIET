15539b769302eea4c90240a600daa6c36f788c3d
Felix Lange <rayon-zunder.0u@icloud.com>
2024-03-05 14:03:20 +0900

    link to edit profile

--- Gemfile
+++ Gemfile
@@ -89,4 +89,3 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
-gem "sassc-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -72,10 +72,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_025701) do
     t.datetime "updated_at", null: false
     t.string "vegan"
     t.string "english_name"
-    t.text "english_description"
     t.string "pescetarian"
     t.string "dairy_free"
     t.string "peanut_free"
+    t.text "english_description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,9 +86,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_025701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false, null: false
     t.string "username"
     t.string "preferences"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
