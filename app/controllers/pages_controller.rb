67ac53a756bf17c884be4062e9dffc7ad4895b92
Felix Lange <rayon-zunder.0u@icloud.com>
2024-03-06 16:55:08 +0900

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,22 +4,4 @@ class PagesController < ApplicationController
   def home
     @food = Food.new
   end
-
-  # def changepreferences
-  # end
-
-  # def updatepreferences
-  #   if current_user.update(user_params)
-  #     redirect_to root_path
-  #   else
-  #     render 'changepreferences'
-  #   end
-  # end
-
-  # private
-
-  # def user_params
-  #   params[:changepreferences][:preferences] = params[:changepreferences][:preferences].join(" ")
-  #   params.require(:changepreferences).permit(:preferences)
-  # end
 end
