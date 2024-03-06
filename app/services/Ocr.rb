f1067938b36f6fb0c1d33a4eed8581bbc36300b2
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-03-06 20:37:08 +0900

    Fixes ocr for fish bento and adds a few more ingredients to the database

--- app/services/Ocr.rb
+++ app/services/Ocr.rb
@@ -21,28 +21,28 @@ end
 private
 
 def cleanup(text)
-  # raise
   text = text.delete("\n").delete("\s").delete(":").delete("●")
   text = text.gsub("、 ", ",")
   text = text.gsub("、", ",")
   original = text.gsub("､", ",")
   remove_before = original.gsub(/(.*)?(原材料名)\s?(称?ポテトチップス称?)?/, "")
   remove_after = remove_before.gsub(/(\((一部に).*含む\).*)?(内容量.*)?/, "")
-  # raise
   text = remove_after.gsub(/(\((カナダ)?(国産)?又はアメリカ\))/, "")
   text = text.gsub(/(\/?調味料\(アミノ酸等)/, "")
   text = text.gsub(/(国内製造)/, "")
   text = text.gsub(/(酸化防止剤\(V.E\))/, "ビタミンE")
+  text = text.gsub(/(酸化防止剤\(V.C\))/, "ビタミンC")
   text = text.gsub(/(国産米)/, "")
-  text = text.delete(")").delete("(")
-  # text = text.gsub(/(pH調整剤)/, "")
+  text = text.gsub(/(\/?酢酸Na)/, "")
+  text = text.gsub(/(pH調整剤,?)/, "")
+  text = text.gsub(/(増粘剤\(.*\))/, "増粘剤")
+  text = text.gsub(/(着色料\(.*\))/, "")
   # text = text.gsub(/(スープ)/, "")
   # text = text.gsub(/(\(分別生産流通管理済み\))/, "")
   # text = text.gsub(/(かやく)/, "")
   # text = text.gsub(/(着色料)/, "")
   # text = text.gsub(/(甘味料)/, "")
   # text = text.gsub(/(乳化剤)/, "")
-  # text = text.gsub(/(増粘剤)/, "")
   # text = text.gsub(/(輸入)/, "")
   # text = text.gsub(/(糖類)/, "")
   # text = text.gsub(/(\/糊料)/, "")
@@ -50,11 +50,11 @@ def cleanup(text)
   # text = text.gsub(/(たん白加水分解物)/, "")
   # text = text.gsub(/(焙煎香辛料)/, "")
   # text = text.gsub(/(外国製造)/, "")
+  text = text.delete(")").delete("(")
   text = text.gsub("・", ",")
   text = text.gsub("/", ",")
   text = text.split(",")
   text = text.uniq
   text = text.join(", ")
-  # raise
   return text
 end
