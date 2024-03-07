c848953a746158a0f5c864753de3e949aea1dd48
Felix Lange <rayon-zunder.0u@icloud.com>
2024-03-07 20:14:16 +0900

    last changes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,18 @@ class Favorite < ApplicationRecord
   belongs_to :food
 
   validates :food, uniqueness: :true
+
+  def check(preference)
+    if preference == "vegan"
+      return self.vegan
+    elsif preference == "vegetarian"
+      return self.vegetarian
+    elsif preference == "pescetarian"
+      return self.pescetarian
+    elsif preference == "dairy-free"
+      return self.dairy_free
+    else
+      return self.peanut_free
+    end
+  end
 end
